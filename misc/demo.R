85868702ce1d79e38f6f4205dbd8059220e58a72
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-06-01 09:28:37 -0700

    Update misc/demo.R

--- misc/demo.R
+++ misc/demo.R
@@ -1,21 +1,16 @@
-# demo.R
-
 library(jNSMR)
 
 # read single-station XML file
-input <- NewhallDatasetFromPath("inst/extdata/WILLIAMSPORT_1930_1930_input.xml")
-
-## TODO: create single or multi-station data from an R data.frame (equivalent to batch CSV)
-# input <- NewhallDataset(data)
+input <- NewhallDatasetFromPath("misc/WILLIAMSPORT_1930_1930_input.xml")
 
 # run model
 output <- run_simulation(input)
 
-# inspect results
-newhall_XML_string_export(input, output)
+# inspect results as string
+cat(newhall_XML_string_export(input, output))
 
 # write XML results to file
-newhall_XML_export("inst/extdata/WILLIAMSPORT_1930_1930_export2.xml", input, output)
+newhall_XML_export("misc/WILLIAMSPORT_1930_1930_export.xml", input, output)
 
 # convenience method to open GUI
 openJAR()
