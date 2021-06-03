69f6452b0d2b4e101154cc5b8f68f91ece67697d
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-06-03 09:47:57 -0700

    newhall_batch: vectorize pathname interface for multiple CSV input in one call

--- R/NewhallBatch.R
+++ R/NewhallBatch.R
@@ -1,24 +1,38 @@
 #' Run a Newhall simulation for each row in a CSV file
-#' @param .data a data.frame
-#' @param pathname path to CSV file with Newhall batch format and required columns
+#' @param .data a _data.frame_ or _character_ vector of paths (used as `pathname`)
+#' @param pathname paths to CSV file with Newhall batch format and required columns; default `NULL`
 #' @param verbose print message about number of simulations and elapsed time
 #' @seealso csv_writeNewhallBatch
-#' @return a `data.frame` containing list columns with Java Objects for _NewhallDataset_, _NewhallResults_, and the column `output` which is _character_ containing the `toString()` output from _NewhallResults_
+#' @return a _data.frame_ containing list columns with Java Objects for _NewhallDataset_, _NewhallResults_, and the column `output` which is _character_ containing the `toString()` output from _NewhallResults_
 #' @export
 #'
-#' @importFrom data.table data.table
+#' @importFrom data.table data.table rbindlist
 newhall_batch <- function(.data = NULL, pathname = NULL, verbose = TRUE) {
 
    # for NSE in data.table / R CMD check
 allAirTempsDbl <- NULL; allPrecipsDbl <- NULL; awc <- NULL; cntryCode <- NULL; dataset <- NULL; elev <- NULL; latDD <- NULL; lonDD <- NULL; pApr <- NULL; pAug <- NULL; pDec <- NULL; pFeb <- NULL; pJan <- NULL; pJul <- NULL; pJun <- NULL; pMar <- NULL; pMay <- NULL; pNov <- NULL; pOct <- NULL; pSep <- NULL; pdEndYr <- NULL; pdStartYr <- NULL; results <- NULL; stationName <- NULL; tApr <- NULL; tAug <- NULL; tDec <- NULL; tFeb <- NULL; tJan <- NULL; tJul <- NULL; tJun <- NULL; tMar <- NULL; tMay <- NULL; tNov <- NULL; tOct<- NULL; tSep <- NULL
 
-  if (is.null(.data)) {
-    stopifnot(!is.null(pathname) && is.null(.data))
-    .data <- csv_readNewhallBatch(pathname)
-  } else {
-    # minimum dataset includes all of the codes specified in colnames of batch file template
-    stopifnot(all(.colnamesNewhallBatch() %in% colnames(.data)))
+  # if .data not specified (NULL), pathname required
+  if (is.null(.data) && !is.null(pathname)) {
+    .data <- pathname
   }
+
+  # if .data is character, it could specify multiple CSVs
+  if (is.character(.data)) {
+    # create aggregate table from character vector >1 
+    .data <- data.table::rbindlist(lapply(.data, function(x) {
+        if (all(nchar(x) > 0) && file.exists(x))
+          csv_readNewhallBatch(x)
+      }), fill = TRUE)
+  } else {
+    stop("'.data' should be a `data.frame` or a vector of path names to CSV files", call. = FALSE)
+  }
+  
+# otherwise, .data inherits from data.frame
+  stopifnot(inherits(.data, 'data.frame'))
+  
+  # minimum dataset includes all of the codes specified in colnames of batch file template
+  stopifnot(all(.colnamesNewhallBatch() %in% colnames(.data)))
 
   batchframe <- data.table::data.table(.data)
   batchframe <- batchframe[batchframe[, list(allPrecipsDbl = list(c(tJan,tFeb,tMar,tApr,tMay,tJun,tJul,tAug,tSep,tOct,tNov,tDec)),
