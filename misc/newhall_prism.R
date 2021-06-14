351d5e0a3320e5ea16efffbfde5f412e616f8a62
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-06-14 14:20:54 -0700

    Clean up PRISM demo

--- misc/newhall_prism.R
+++ misc/newhall_prism.R
@@ -6,12 +6,11 @@ PRISM_PATH <- "~/Geodata/PRISM"
 # PRISM resolution; either 4km or 800m
 RESOLUTION <- "4km"
 
-# get the monthly files only (hardcoded for 30yr normals for now)
-bilfile <- list.files(PRISM_PATH, "\\.bil$", recursive = TRUE)
-
 # only download the PRISM data if it can't be found in PRISM_PATH
 if (length(bilfile) == 0) {
   source("misc/dl_PRISM.R") # download .BIL files if needed
+  # get the monthly files only (hardcoded for 30yr normals for now)
+  bilfile <- list.files(PRISM_PATH, "\\.bil$", recursive = TRUE)
 }
 
 # capture file name components
@@ -52,8 +51,8 @@ prism_frame$stProvCode <- ""
 prism_frame$notes <- ""
 prism_frame$stationID <- 1:nrow(prism_frame)
 
-# make test batch of prism point data values s
-test_set <- prism_frame[round(runif(10000, 1, nrow(prism_frame))),]
+# make test batch of prism point data values 
+test_set <- prism_frame[round(runif(20000, 1, nrow(prism_frame))),]
 
 # write to file
 write.csv(test_set, file = "misc/prism_monthly.csv")
@@ -61,16 +60,21 @@ write.csv(test_set, file = "misc/prism_monthly.csv")
 # read batch file, run simulations
 res <- jNSMR::newhall_batch(pathname = "misc/prism_monthly.csv")
 
-system.time(res2 <- data.table::data.table(res)[, .format_results(results[[1]]), by = list(1:nrow(res))])
-
-newsp <- sf::st_as_sf(cbind(test_set[,c('lonDD','latDD')], res2), coords = c('lonDD','latDD'))
+newsp <- sf::st_as_sf(cbind(test_set[,c('lonDD','latDD')], res), coords = c('lonDD','latDD'))
+sf::st_crs(newsp) <- sf::st_crs(4326)
 plot(newsp$geometry, col=factor(newsp$TemperatureRegime))
 plot(newsp$geometry, col=factor(newsp$MoistureRegime))
 plot(newsp$geometry, col=factor(newsp$RegimeSubdivision1))
 plot(newsp$geometry, col=factor(newsp$RegimeSubdivision2))
 
 newspsub <- subset(newsp, MoistureRegime %in% c("Xeric","Aridic","Ustic","Udic"))
-plot(newspsub$geometry, col=factor(newspsub$MoistureRegime), pch=".", cex=5)
+legnames <- factor(paste(newspsub$MoistureRegime))
+nlegnames <- unique(levels(legnames))
+nlegcolors <- rev(viridisLite::viridis(length(nlegnames)))
+plot(sf::st_transform(newspsub, sf::st_crs(6350))$geometry, 
+     col = nlegcolors[match(legnames, nlegnames)], pch=".", cex=5)
+plot(sf::st_transform(spData::us_states$geometry, sf::st_crs(6350)), add=T)
+legend("topright", legend = nlegnames, pch=".", fill = nlegcolors)
 
 table(newspsub$RegimeSubdivision1)
 
@@ -78,7 +82,8 @@ newspsub2 <- subset(newsp, MoistureRegime %in% c("Aridic","Xeric"))
 legnames <- factor(paste(newspsub2$RegimeSubdivision1, newspsub2$MoistureRegime))
 nlegnames <- unique(levels(legnames))
 nlegcolors <- viridisLite::viridis(length(nlegnames))
-plot(newspsub2$geometry, col = nlegcolors[match(legnames, nlegnames)], pch=".", cex=5)
-plot(spData::us_states$geometry, add=T)
+plot(sf::st_transform(newspsub2, sf::st_crs(6350))$geometry, 
+     col = nlegcolors[match(legnames, nlegnames)], pch=".", cex=5)
+plot(sf::st_transform(spData::us_states$geometry, sf::st_crs(6350)), add=T)
 legend("bottomleft", legend = nlegnames, pch=".", fill = nlegcolors)
 
