eb043351c6f34d47d5c38c18ce37488f88928928
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2022-01-05 13:58:56 -0800

    I think this works now...

--- misc/job800.R
+++ misc/job800.R
@@ -2,39 +2,41 @@
 library(terra)
 library(jNSMR)
 
-test <- FALSE 
+test <- FALSE
 r <- terra::rast("prism_in_800.tif")
 r <- aggregate(r, 20)
 
-# # NA-masked static AWC
-v <- rep(NA, ncell(r))
-v[!is.na(values(r$pJan))] <- 10
+# # # NA-masked static AWC
+# v <- rep(NA, ncell(r))
+# v[!is.na(values(r$pJan))] <- 100
+# values(r$awc) <- v
 
 # ISSR-800 AWC
-# download.file("https://soilmap2-1.lawr.ucdavis.edu/800m_grids/rasters/water_storage.tif", destfile = "water_storage.tif")
-# awc <- rast("water_storage.tif")
-# awc <- project(awc, r)
-# # global mean is near the default newhall value of 200mm
-# mean(values(awc)*10, na.rm=T)
-# #> [1] 197.82760 # 200 (mm water) is default
+# download.file(
+#   "https://soilmap2-1.lawr.ucdavis.edu/800m_grids/rasters/water_storage.tif",
+#   destfile = "water_storage.tif",
+#   mode = "wb"
+# )
+awc <- rast("water_storage.tif")
+terra::crs(awc) <- "EPSG:5070"
+awc <- project(awc, r)
 
-# testing blocks of missing data
-# awc <- raster::raster("water_storage.tif")
-# crs(awc) <- "EPSG:4326"
-# awc <- project(awc, r)
-# v <- values(awc)
-v[1:45000] <- NA
-# values(awc) <- v
+# global mean is near the default newhall value of 200mm
+mean(values(awc) * 10, na.rm = TRUE)
+#> [1] 197.82760 # 200 (mm water) is default
 
-# 
 # # +/- 1 standard deviation ~[100,300]mm
 # sd(values(awc)*10, na.rm=T)
 # #> [1] 97.52501
-# 
+ 
 # # convert cm -> mm?
-# v <- values(awc) * 10 
-
+v <- values(awc) * 10
+r$awc <- rep(NA, ncell(r))
 values(r$awc) <- v
+
+# testing blocks of missing data
+# v[1:45000] <- NA
+# values(awc) <- v
 
 plot(r$awc)
 
@@ -47,13 +49,14 @@ quantile(values(r2$awc), na.rm = TRUE)
 
 r2$stationName <- NULL; r2$stationID <- NULL; r2$notes <- NULL; r2$stProvCode <- NULL; r2$netType <- NULL; r2$cntryCode <- NULL; r2$pdEndYr <- NULL; r2$pdStartYr <- NULL; r2$pdType <- NULL; r2$maatmast <- NULL
 
-# system.time(resbig <- newhall_batch(r2))
-system.time(resbig <- newhall_batch(r2, cores = 6, nrows = ifelse(test, 40, 2)))
+#system.time(resbig <- newhall_batch(r2))
+system.time(resbig <- newhall_batch(r2, cores = 3, nrows = ifelse(nrow(r2) * ncol(r2) > 100000, 2, 40)))
 
-terra::writeRaster(resbig, filename = "newhall_800m_100mmAWS.tif", overwrite = TRUE)
+terra::writeRaster(resbig, filename = "newhall_800m_ISSRAWS.tif", overwrite = TRUE)
 
 # 44% of the full raster is NA
 # sum(is.na(values(r$pJan))) / ncell(r)
+
 plot(resbig$waterHoldingCapacity)
 plot(resbig$temperatureRegime)
 plot(resbig$moistureRegime)
