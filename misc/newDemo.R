dcd99d7691a695846fb9c51fb3f8d9ec14987273
Andrew G. Brown <andrew.g.brown@usda.gov>
2023-10-02 15:33:35 -0700

    demo

--- misc/newDemo.R
+++ misc/newDemo.R
@@ -3,22 +3,21 @@ library(soilDB)
 library(terra)
 library(rgeedim)
 
-x <- fetchSDA_spatial(c("CA067", "CA628", "CA731", "CA729", "CA732"), 
+x <- vect(fetchSDA_spatial(c("CA067", "CA628", "CA731", "CA729", "CA732"),
                       by.col = "areasymbol",
-                      geom.src = "sapolygon")
+                      geom.src = "sapolygon"))
 
 # get PRISM climate and SSURGO-derived available water storage data
-predictors <- c(newhall_prism_extent(x),
-                newhall_issr800_extent(x))
+predictors <- c(newhall_prism_subset(x), newhall_issr800_subset(x))
 
 # take focal window to smooth/remove NoData in AWC
-predictors$awc <- focal(predictors$awc, 5, na.rm=TRUE)
+predictors$awc <- focal(predictors$awc, 5, fun="mean", na.rm=TRUE)
 
 # use Lang et al. (2023) canopy height <https://www.nature.com/articles/s41559-023-02206-6>
 gd_initialize()
 
-gd_image_from_id('users/nlang/ETH_GlobalCanopyHeight_2020_10m_v1') |> 
-  gd_download(region = as.polygons(vect(x), ext = TRUE),
+gd_image_from_id('users/nlang/ETH_GlobalCanopyHeight_2020_10m_v1') |>
+  gd_download(region = as.polygons(x, ext = TRUE),
               bands = list("b1"),
               filename="canopy_height.tif",
               scale = 100)
@@ -33,8 +32,8 @@ predictors$hasOHorizon <- project(h, predictors) > 8
 plot(predictors$hasOHorizon)
 
 # add elevation
-gd_image_from_id('USGS/NED') |> 
-  gd_download(region = as.polygons(vect(x), ext = TRUE),
+gd_image_from_id('USGS/NED') |>
+  gd_download(region = as.polygons(x, ext = TRUE),
               bands = list("elevation"),
               filename="elev.tif",
               scale = 100)
@@ -42,16 +41,16 @@ e <- rast("elev.tif")
 predictors$elev <- project(e, predictors)
 
 # run batches of models on gridded data
-res <- newhall_batch(predictors, soilAirOffset = 2, core_thresh = 1e4, cores = 8)
+res <- newhall_batch(crop(predictors, ext(predictors)), cores=8, core_thresh = 1e5)
 
-y <- fetchSDA_spatial(SDA_spatialQuery(predictors, "areasymbol")$areasymbol, 
-                      by.col = "areasymbol", 
+y <- fetchSDA_spatial(SDA_spatialQuery(predictors, "areasymbol")$areasymbol,
+                      by.col = "areasymbol",
                       geom.src = "sapolygon")
 # inspect one modeled soil taxonomic property
 plot(
   res$dryDaysAfterSummerSolstice,
   main = "dryDaysAfterSummerSolstice",
-  col = grDevices::hcl.colors(10, "cividis")
+  col = grDevices::hcl.colors(50, "cividis")
 )
 plot(as.lines(vect(y)), add = TRUE, col = "darkorange")
 
